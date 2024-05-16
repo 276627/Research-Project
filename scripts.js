3694bfb7545094a6fd082cbef20d653f21e50290
276627 <157831352+276627@users.noreply.github.com>
2024-05-16 11:12:05 -0500

    may16

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// script.js
+
+function playVideo() {
+    var videoWrapper = document.getElementById("videoWrapper");
+    var video = document.getElementById("youtubeVideo");
+    video.src = "https://www.youtube.com/embed/_wloMt3OLqo?autoplay=1";
+    videoWrapper.style.display = "block";}
